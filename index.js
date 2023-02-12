798fdaafdea58bfaa0baf2ac779112262f004646
Fahim-Ullah123 <121448854+Fahim-Ullah123@users.noreply.github.com>
2023-02-12 23:42:21 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function openMenu (){
+    document.getElementById("responsive").style.top="3rem";
+    document.getElementById("showMenu").style.display="none";
+    document.getElementById("hideMenu").style.display="block";
+}
+
+function closeMenu (){
+    document.getElementById("responsive").style.top="-16rem";
+    document.getElementById("showMenu").style.display="block";
+    document.getElementById("hideMenu").style.display="none";
+} 
